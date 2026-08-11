3c040a650fea3aa73e49300e6278c5b42020178d
Ghiwane <sehaki.ghiwane@gmail.com>
2026-08-11 16:51:54 +0100

    Implement Q-Learning agent and training loop for Taxi environment (Gymnasium)

---
+++ Taxi/agent.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random
+
+class QLearningAgent():
+    """Q-Learning agent for tabular reinforcement learning."""
+
+    def __init__(self, n_states, n_actions):
+        """Initialize hyperparameters and Q-table."""
+        self.alpha = 0.5              # Learning rate
+        self.gamma = 0.95              # Discount factor
+        self.eps = 1.0                # Initial exploration rate (epsilon)
+        self.eps_min = 0.1            # Minimum exploration rate
+        self.eps_decay = 0.99        # Epsilon decay factor per episode
+        self.n_actions = n_actions    # Number of actions
+        self.q_table = np.zeros((n_states, n_actions)) # Q-table initialized with zeros: (state, actions)
+
+    def choose_action(self, state):
+        """Select an action using the epsilon-greedy policy."""
+        rand_nb = np.random.rand()
+        if rand_nb < self.eps:
+            return random.choice(range(self.n_actions)) # Explore: select a random action
+        else:
+            return np.argmax(self.q_table[state]) # Exploit: select the best action from Q-table
+
+    def update(self, state, action, reward, next_state, done):
+        """Update Q-value for a state-action pair using TD learning rule."""
+        current_q = self.q_table[state, action] # Current Q-value
+        if done:
+            target = reward # Terminal state: no future rewards
+        else: 
+            target = reward + self.gamma * np.max(self.q_table[next_state]) # Bellman target
+
+        td_error = target - current_q # Temporal Difference (TD) error
+        self.q_table[state][action] = current_q + self.alpha * td_error # Update Q-value
+
+    def decay_epsilon(self):
+        """Decay the exploration rate (epsilon) after each episode."""
+        self.eps = max(self.eps_min, self.eps * self.eps_decay)
---
+++ Taxi/train.py
@@ -0,0 +1,65 @@
+import gymnasium as gym
+from agent import QLearningAgent
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+
+# Initialize the environment and Q-Learning agent
+env = gym.make("Taxi-v4", render_mode="ansi")
+
+n_states = env.observation_space.n
+n_actions = env.action_space.n
+
+agent = QLearningAgent(n_states, n_actions) 
+
+episode_rewards = []  # List to store cumulative reward for each episode
+episodes = 750       # Total number of training episodes
+
+state, _ = env.reset(seed=SEED)
+
+# Main training loop over episodes
+for e in range(episodes):
+    state, _ = env.reset()       # Reset environment to initial state
+    done = False
+    total_rewards = 0        # Accumulator for total reward in the current episode
+
+    # Step-by-step episode loop
+    while not done:
+        action = agent.choose_action(state)                     # Select action using epsilon-greedy policy
+        next_state, reward, terminated, truncated, info = env.step(action)            # Execute action in environment
+        done = terminated or truncated                          #Check if an episode is done
+        agent.update(state, action, reward, next_state, done)   # Update Q-table with step outcome
+        state = next_state                                      # Transition to next state
+        total_rewards += reward                                 # Accumulate step reward
+
+    agent.decay_epsilon()                  # Decay exploration rate (epsilon) after each episode
+    episode_rewards.append(total_rewards)  # Record total episode reward
+
+    # Log training progress every 50 episodes
+    if e % 50 == 0:
+        print(f'Episode : {e} | Reward : {total_rewards} | Current epsilon : {agent.eps}')
+
+
+# --- MATPLOTLIB PLOT ---
+window_size = 100  # Window size for calculating the moving average
+
+# Compute rolling average using 1D convolution
+rolling_avg = np.convolve(episode_rewards, np.ones(window_size) / window_size, mode='valid')
+
+# Plot the smoothed learning curve (moving average)
+plt.plot(range(window_size - 1, episodes), rolling_avg, color='blue', linewidth=2, label=f'Moyenne glissante ({window_size} épisodes)')
+
+# Configure plot title, axis labels, reference line, legend, and grid
+plt.title('Training Q-Learning - Taxi-v4')
+plt.xlabel('Episode')
+plt.ylabel('Total Reward')
+plt.axhline(y=5, color='r', linestyle='--', alpha=0.5)  # Reference benchmark line
+plt.legend()
+plt.grid(True, linestyle=':', alpha=0.6)
+
+# Render and display the plot window
+plt.show()
