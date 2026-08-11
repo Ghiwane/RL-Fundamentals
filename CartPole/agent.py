f1fa585a48a268429eaca33a2d25ccd0bb1befb6
Ghiwane <sehaki.ghiwane@gmail.com>
2026-08-11 19:14:44 +0100

    Implement CartpoleAgent class for Q-Learning in CartPole environment

---
+++ CartPole/agent.py
@@ -0,0 +1,50 @@
+from network import QNetwork
+from replay_buffer import ReplayBuffer
+import torch
+import numpy as np
+import random
+import torch.nn.functional as F
+
+class CartpoleAgent():
+    def __init__(self, state, action):
+        self.q_network = QNetwork(state, action)
+        self.target_network = QNetwork(state, action)
+
+        self.target_network.load_state_dict(self.q_network.state_dict())
+
+        self.optimizer = torch.optim.Adam(self.q_network.parameters(), lr=0.001)
+
+        self.buffer = ReplayBuffer(capacity=10000)
+
+        self.gamma = 0.95
+        self.eps = 1.0 
+        self.eps_min = 0.1
+        self.eps_decay = 0.999
+        self.batch_size = 64
+        self.n_actions = action
+
+    def choose_action(self, state):
+        with torch.no_grad():
+            rand_nb = np.random.rand()
+            if rand_nb < self.eps:
+                return random.choice(range(self.n_actions))
+            else:
+                state_tensor = torch.tensor(state)
+                return np.argmax( self.q_network(state_tensor))
+
+    def train_step(self):
+        if len(self.buffer) < self.batch_size:
+            return
+        states, actions, rewards, next_states, dones = self.buffer.sample(self.batch_size)
+        q_values = self.q_network(states)
+        selected_q_values = q_values.gather(1, actions)
+
+        with torch.no_grad():
+            target = rewards + self.gamma * max(self.target_network(next_states)) * (1 - dones)
+            target = target.unsqueeze(1)
+
+            loss = F.mse_loss(selected_q_values, target)
+
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
